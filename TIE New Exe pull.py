18900b9322fb033396817cc217b6eade83d9859c
ColbyBurkett <burkettc@gmail.com>
2017-04-16 21:22:26 -0700

    Add files via upload

--- TIE New Exe pull.py
+++ TIE New Exe pull.py
@@ -1,7 +1,19 @@
+# This script runs and listens for new PE launches reported by TIE.
+# When the launch is seen, it instructs the client to copy that binary
+# to another location. This location is arbitrary, but should be a write-only
+# UNC path
+
 import os
 import sys
+import logging
 import mcafee
 import urlquote
+import time
+import json
+
+from dxlclient.client import DxlClient
+from dxlclient.client_config import DxlClientConfig
+from dxltieclient import TieClient, FirstInstanceCallback
 
 from dxlclient.client_config import DxlClientConfig
 from dxlclient.client import DxlClient
@@ -19,54 +31,95 @@ logger = logging.getLogger(__name__)
 config = DxlClientConfig.create_dxl_config_from_file(CONFIG_FILE)
 
 # Connect to ePO with WebAPI
-mc = mcafee.client('10.10.0.10','8443','podadmin','MFEpoc92!','https','json')
-ePOTag = 'Suspect'
+mc = mcafee.client('192.168.11.103','8443','admin','IsecG123!','https','json')
+#ePOTag = 'Suspect'
 
+class MyFirstInstanceCallback(FirstInstanceCallback):
+    """
+    My first instance callback
+    """
+    def on_first_instance(self, first_instance_dict, original_event):
+        # Display the DXL topic that the event was received on
+        print "First instance on topic: " + original_event.destination_topic
+
+        # Dump the dictionary
+        print json.dumps(first_instance_dict,
+                         sort_keys=True, indent=4, separators=(',', ': '))
+        agentGuid = json.dumps(first_instance_dict['agentGuid'])[1:-1]
+        fileName = json.dumps(first_instance_dict['name'])[1:-1]
+        sha1 = json.dumps(first_instance_dict['hashes']['sha1'])[1:-1]
+        print agentGuid, fileName, sha1
+        searchHASH(sha1,agentGuid,fileName)
+        
 # Create the client
-with DxlClient(config) as client:
+client1 = DxlClient(config)
+client2 = DxlClient(config)
 
-    # Read in hashes from file
-    hashes = []
-    hashesFile = open('bad-hashes.txt', 'r')
-    for rowofdata in hashesFile:
-        rowofdata = rowofdata.strip('\r\n')
-        rowofdata = list(rowofdata.split(','))
-        hashes.append(rowofdata[1])
-    hashesFile.close()
+# Connect to the fabric
+client1.connect()
+client2.connect()
 
-    # Connect to the fabric
-    client.connect()
+# Create the McAfee Active Response (MAR) client
+mar_client = MarClient(client1)
 
-    # Create the McAfee Active Response (MAR) client
-    mar_client = MarClient(client)
+# Create the McAfee Threat Intelligence Exchange (TIE) client
+tie_client = TieClient(client2)
 
-    for hashitem in hashes:
-        # Performs the search
-        # Execute the search
-        print "Searching for hash: "+hashitem
-        result_context = mar_client.search(
-                projections=[{
-                        "name": "HostInfo",
-                        "outputs": ["hostname"]
-                            }],
-                conditions={
-                    "or": [{
-                        "and": [{
-                            "name": "Files",
-                            "output": "sha1",
-                            "op": "EQUALS",
-                            "value": hashitem
-                        }]
+# Create first instance callback
+first_instance_callback = MyFirstInstanceCallback()
+
+# Register first instance callback with the client
+tie_client.add_file_first_instance_callback(first_instance_callback)
+
+def searchHASH(sha1,agentGuid,fileName):
+    # Performs the search
+    # Execute the search
+    print "Searching for hash: "+sha1
+    result_context = mar_client.search(
+            projections=[{
+                    "name": "Files",
+                    "outputs": ["full_name"]
+                        }],
+            conditions={
+                "or": [{
+                    "and": [{
+                        "name": "Files",
+                        "output": "sha1",
+                        "op": "EQUALS",
+                        "value": sha1
                     }]
-                }
-            )
+                }]
+            }
+        )
 
-        # Loop and display the results
-        if result_context2.has_results:
-            search_result = result_context.get_results(limit=10)
-            print "Hash "+hashitem+" found on:"
-            for item in search_result["items"]:
-                print item["output"]['HostInfo|hostname']
-                systems = mc.system.find(item["output"]['HostInfo|hostname'])
-                for system in systems:
-                    mc.system.applyTag(system['EPOComputerProperties.ComputerName'],ePOTag)
+    # Loop and display the results
+    if result_context.has_results:
+        search_result = result_context.get_results(limit=1)
+        print "File found:"
+        for item in search_result["items"]:
+            filePath = item["output"]['Files|full_name']
+            print filePath
+            fileOut = agentGuid[1:-1]
+            #fileOut = "test"
+            f=open(fileOut,"w+")
+            f.write(filePath)
+            f.close()
+            task = mc.clienttask.find('CollectFile')[0]
+            taskId=task['objectId']
+            productId=task['productId']
+            systems = mc.system.find(agentGuid[1:-1])
+            for system in systems:
+                #mc.system.applyTag(system['EPOComputerProperties.ComputerName'],ePOTag)
+                mc.clienttask.run(ids=system['EPOComputerProperties.ParentID'],taskId=taskId,productId=productId)
+                #print system
+                result_context = mar_client.search(
+                        projections=[{
+                                "name": "CollectFile",
+                                "outputs": ["status"]
+                                    }])
+                print result_context
+
+# Wait forever
+print "Waiting for first instance events..."
+while True:
+    time.sleep(60)
