c743a82540bccf9b9195842590c6c3076630b87d
ColbyBurkett <burkettc@gmail.com>
2017-04-16 21:23:20 -0700

    Add files via upload

--- ClientPull.ps1
+++ ClientPull.ps1
@@ -9,30 +9,33 @@ $PullRequest = $line[1]
 
 # Check Web Server for file with same name as Agent GUID
 # This file will contain a single line of file to be copied to new location
-# Recommend http server that can scale well
+# Recommend http server that can scale well and is available to all Corporate hosts
 # Testing can be done with Python
 
 # Change "http://127.0.0.1:8000/" to your preferred host
-if ($PullRequest -eq "N") {
+if ($PullRequest -eq "Y") {
 	$url = "http://127.0.0.1:8000/" + $MAGuid
-	$output = "fileToPull"
+	$output = "C:\Users\Administrator\Desktop\"+"fileToPull"
 	$start_time = Get-Date
 
 	Invoke-WebRequest -Uri $url -OutFile $output
 	Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 	
-	$filePull = Get-Content fileToPull
+	$filePull = Get-Content $output
 }
 
 # Reset the Custom Property to disable polling the Web Server when collector is executed
 & "C:\Program Files\McAfee\Agent\maconfig" -custom -prop1 "N"
 
 # Set the destination naming scheme.
-$fileDest = $MAGuid + ".pulled"
+$fileDest = "C:\Users\Administrator\Desktop\"+ $MAGuid + ".pulled"
 
 # Copy file to new location
 # Recommand a write-only UNC file share
 Copy-Item -Path $filePull -Destination $fileDest
 
 # Clean up after operation
-Remove-Item fileToPull
+Remove-Item $output
+
+# Give some output to satisfy MAR
+Write-Host "Done"
